ee8287da32029b66e12d8aa104a2e31760db7402
GitHub Action <action@github.com>
2024-06-14 17:17:05 +0000

    Update to 0.11.6

--- asd.rb
+++ asd.rb
@@ -2,7 +2,7 @@ class Asd < Formula
   desc "Reads ALPS documents and produces a full state diagram and hyperlinked documentation."
   homepage "https://alps-asd.github.io/"
   url "https://github.com/alps-asd/app-state-diagram/releases/download/0.11.6/asd.phar"
-  sha256 "e14842871b7a442f8931f7099aac4588a2806a7bb09b750997d834eef8e29edb"
+  sha256 ""
   license "MIT"
 
   depends_on "php@8.3"
