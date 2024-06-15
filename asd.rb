54025debf09efe13e18ec3b9147b5bdf3ff42194
Akihito Koriyama <akihito.koriyama@gmail.com>
2024-06-16 03:50:56 +0900

    Refactor installation process in asd.rb
    The installation process in asd.rb has been revamped to include the extraction of PHAR files, running npm install, providing execution permissions to necessary files and creating asd.phar execution scripts. Additionally, the previous test for expected output has been replaced with new tests to execute asdw and asd scripts.

--- asd.rb
+++ asd.rb
@@ -9,12 +9,42 @@ class Asd < Formula
   depends_on "composer" => :build
   depends_on "node"
 
-  def install
-     bin.install "asd.phar"
-     mv bin/"asd.phar", bin/"asd"
-  end
 
-  test do
-      assert_match "Expected output", shell_output("#{bin}/asd")
-  end
+  def install
+      # PHARファイルをlibexecにインストール
+      libexec.install "asd.phar"
+
+      # PHARファイルを解凍
+      system "php -r \"(new Phar('#{libexec}/asd.phar'))->extractTo('#{libexec}');\""
+
+      # npm install の実行
+      system "npm", "install", "--prefix", "#{libexec}/asd-sync"
+
+      # 必要なファイルに実行権限を付与し、shebangを追加
+      bin_asd = "#{libexec}/bin/asd"
+      chmod 0755, bin_asd
+      File.open(bin_asd, 'r+') do |file|
+        content = file.read
+        file.seek(0)
+        file.write("#!/usr/bin/env php\n" + content)
+      end
+
+      # npmプロジェクト実行スクリプトの作成
+      (bin/"asdw").write <<~EOS
+        #!/bin/bash
+        cd "#{libexec}/asd-sync" && npm start -- --profile "$@"
+      EOS
+      (bin/"asdw").chmod 0755
+
+      # asd.phar 実行スクリプトの作成
+      (bin/"asd").write <<~EOS
+        #!/bin/bash
+        php "#{libexec}/asd.phar" "$@"
+      EOS
+      (bin/"asd").chmod 0755
+    end
+    test do
+        system "#{bin}/asdw"
+        system "#{bin}/asd"
+    end
 end
